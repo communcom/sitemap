6b2dad1a3a18adeb06325f014bc65d98de441387
Alexey Elizarov <felex-ae@ya.ru>
2020-02-12 20:33:28 +0300

    refactor: change default envs

--- src/data/env.js
+++ src/data/env.js
@@ -2,8 +2,8 @@ const env = process.env;
 
 module.exports = {
     GLS_PRISM_MONGO_CONNECT: env.GLS_PRISM_MONGO_CONNECT,
-    GLS_GENERATE_EVERY: Number(env.GLS_GENERATE_EVERY) || 10 * 1000,
-    GLS_FILL_EVERY: Number(env.GLS_GENERATE_EVERY) || 5000,
+    GLS_GENERATE_EVERY: Number(env.GLS_GENERATE_EVERY) || 30 * 60 * 1000,
+    GLS_FILL_EVERY: Number(env.GLS_GENERATE_EVERY) || 30000,
     GLS_DESTINATION_FOLDER: env.GLS_DESTINATION_FOLDER || './sitemap',
     GLS_HOSTNAME: env.GLS_HOSTNAME || 'https://commun.com',
     GLS_SITEMAP_SIZE: env.GLS_SITEMAP_SIZE || 40000,
