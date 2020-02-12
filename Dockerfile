419372704f99dedeb160207e9af7de3f2b62303f
Alexey Elizarov <felex-ae@ya.ru>
2020-02-12 15:00:07 +0300

    fix: improve

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:10-alpine
+ENV NODE_ENV=production
 WORKDIR /usr/src/app
 COPY ./package*.json ./
 RUN npm install --only=production
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 require('cyberway-core-service').utils.defaultStarter(require('./Main'));
